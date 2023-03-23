03160a66c2cc85261d7d70ceaa3ab59401443483
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-03-23 19:42:17 +0200

    jshw1

---
+++ js/iedx.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+let swordName = "farm";
+console.log(swordName);
+const strinklangs = swordName.length;
+console.log(strinklangs);
+
+const message = "rrrrrrrrr"
+
+
+
---
+++ js/iedx2.js
@@ -0,0 +1,9 @@
+
+const x = 10;
+const t = 99;
+const y = 6;
+console.log(t + x + y * x);
+console.log(t * y);
+console.log(x + y);
+console.log(x % y);
+console.log(x * y);
