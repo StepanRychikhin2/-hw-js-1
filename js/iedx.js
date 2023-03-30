71dd79d099a5248ddf2b931c45682acd43d6e5b3
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-03-30 19:08:32 +0300

    hw2

--- js/iedx.js
+++ js/iedx.js
@@ -1,16 +1,10 @@
-
-
-
-
-
-
-
+//   hw-1
 let swordName = "farm";
 console.log(swordName);
 const strinklangs = swordName.length;
 console.log(strinklangs);
 
-const message = "rrrrrrrrr"
+
 
 
 
--- js/iedx2.js
+++ js/iedx2.js
@@ -1,9 +1,69 @@
+//  n1
+const str = 'Привіт';
+console.log(typeof str);
+const num = 123;
+console.log(typeof num);
+const flag = true;
+console.log(typeof flag);
+const txt = true;
+console.log(typeof txt);
 
-const x = 10;
-const t = 99;
-const y = 6;
-console.log(t + x + y * x);
-console.log(t * y);
-console.log(x + y);
-console.log(x % y);
-console.log(x * y);
+
+
+
+//  n2
+const neme1 =   true;
+console.log(neme1);
+
+const neme2 =  false;
+console.log(neme2);
+
+const neme3 =   true;
+console.log(neme3);
+
+const neme4 =   true;
+console.log(neme4);
+
+const neme5 =  false;
+console.log(neme5);
+
+let neme6 =  0.8;
+console.log(neme6);
+
+let neme7 =  4;
+console.log(neme7);
+let neme8 =  3775.8;
+console.log(neme8);
+
+let neme9 =  3596.8;
+console.log(neme9);
+
+let neme10 =  45000;
+console.log(neme10);
+
+
+
+
+//  n3
+const userneme = prompt("Введіть ваше і'мя");
+console.log(userneme);
+
+
+//  n4
+const frendsAge = 13;
+const meAge = 13;
+const opAge = frendsAge + meAge;
+alert(opAge);
+
+//  n5
+const content = "Я навчаюся в GoITeens в групі:";
+const group = "FE1y_1 менші";
+console.log(content,group);
+
+
+//  dp
+
+const neme = 'MacBook Pro 13';
+const prise = 10000;
+console.log(`Обрано ${neme}, ціна за штуку ${prise} кредитів`);
+
