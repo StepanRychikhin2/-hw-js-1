06a60473fc47be1ef5c529a5f510122c5fe984ec
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-04-06 18:27:43 +0300

    hw3

--- js/iedx.js
+++ js/iedx.js
@@ -1,8 +1,30 @@
-//   hw-1
-let swordName = "farm";
-console.log(swordName);
-const strinklangs = swordName.length;
-console.log(strinklangs);
+// nn1
+console.log(Number.parseInt("5px"));
+console.log(Number.parseInt("12djd334"));
+console.log(Number.parseInt("12.45asdwe12"));
+console.log(Number.parseInt("qwqweeewq"));
+
+
+
+// nn2
+console.log(Number.parseFloat("5px"));
+console.log(Number.parseFloat("12djd334"));
+console.log(Number.parseFloat("12.45asdwe12"));
+console.log(Number.parseFloat("qwqweeewq"));
+
+
+// nn3
+const max = Math.max(2, 34, 99, 3, 22, 36, 733, 18);
+console.log(max);
+
+const min = Math.min(2, 34, 99, 3, 22, 36, 733, 18);
+console.log(min);
+
+// nn4
+const ran1 = 0;
+const ran2 = 20;
+const randoms = Math.random() * (ran2 - ran1) + ran1;
+console.log(randoms);
 
 
 
--- js/iedx3.js
+++ js/iedx3.js
@@ -1,1 +1,6 @@
+//   hw-1
+let swordName = "farm";
+console.log(swordName);
+const strinklangs = swordName.length;
+console.log(strinklangs);
 
