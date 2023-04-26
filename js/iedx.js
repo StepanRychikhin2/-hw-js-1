32d6257221fd9d59c6409b527cab843b66a4ba43
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-04-26 22:14:19 +0300

    50%hv5

--- js/iedx.js
+++ js/iedx.js
@@ -1,70 +1,93 @@
-// ЬЬ1
-const emeil ="linnikadd@gmail.com";
-const provsrca1 = "@";
-const provsrcaemail = "email";
-const oma = emeil.length;
-console.log(emeil.includes(provsrca1));
-console.log(emeil.includes(provsrcaemail));
-console.log(oma);
+// ДД1
+const costFruit = 850000;
+let fruitShop = "???????";
+if (costFruit > 849999) {
+    fruitShop = "Можеш купити фрукт";
+} else {
+    fruitShop = "Недостатньо беілі";
+}
+console.log(fruitShop);
+
+// ДД2
+
+const point = prompt("Яка “офіційна” назва JavaScript?");
+if (point === "ECMAScript") {
+    alert("Правильно!");
+} else {
+    alert("Ви не знаєте? ECMAScript!");
+}
+
+// ДД3
+const x1 = 10;
+const x2 = 30;
+const number = 45;
 
 
 
 
-// ЬЬ2
-const myyy = "My";
-const nameeee = "name";
-const isssss = "is";
-const fullName =  myyy +  " "  + nameeee + " " +  isssss  + " " + "Victor";
-console.log(fullName);
+// ДД4
 
-// ЬЬ3
-const userName = "Stepan";
-const payment = 100;
-const rrr = alert(`Дякуємо, ${userName} До сплати ${payment} гривень`);
-// ЬЬ4
-let meName = "Stepan";
-const ralase = "@";
-const relesee = meName.replace("S","@");
-console.log(relesee);
 
-// ЬЬ5
-const nemeMe = "Степан Ричіхін";
-const checneme = nemeMe.indexOf(" ");
-console.log(checneme);
+const isOnline = false;
+const isFriend = true; 
+const isDnd = false;
+// const canOpenChat = isFriend && isOnline && isDnd;
+// console.log(canOpenChat);
+if(isFriend && isOnline && isDnd === true){
+    alert("Можна поговорити");
+} else {
+    alert("Не можеш поговорити");
+}
 
-// ЬЬ6
-let nile = "ТУТ ЗАРА БУДУ 10 НУЛІВ";
-nile = nile.padEnd(32, "0");
-console.log(nile);
-const enile = nile.length;
-console.log(enile);
 
-// ЬЬ7
-const  result = 5+5+"5";
-console.log(result);
-// ЬЬ8
-const productName = "Droid";
-const pricePerItem = 3500;
-const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// ДД5
 
-// ЬЬ9
-const pricePerDroid = 800;
-const orderedQuantity = 6;
-const deliveryFee = 50;
-const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
-const infomessage = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
-console.log(infomessage);
-// ЬЬ10
-const courseTopic = "JavaScript essentials";
-const courseTopicLength = courseTopic.length;
-const firstElement = courseTopic[0];
-const lastElement = courseTopic[courseTopicLength - 1];
-console.log(courseTopic);
-console.log(courseTopicLength);
-console.log(firstElement);
-console.log(lastElement);
-// ЬЬ11
-const string = "promocode Pro22812";
-const length = 9;
-const sudddd = string.slice(0, length);
-console.log(sudddd);
+
+
+const free = 0;
+const pro = 10;
+const vip = 50;
+const mestatus = 1;
+if(mestatus >= vip){
+    alert("Вітаю ти vip ти маєш доступ");
+}
+else if (mestatus >= pro){
+    alert("Вітаю ти pro ти маєш доступ");
+}
+else if (mestatus >= free){
+alert("Ти не маєш доступу");
+}
+
+
+
+// ДД6
+const hmmm = prompt("Діапазон 55-99");
+const min = 54;
+const max = 100;
+if(hmmm >= max || hmmm < min) { 
+    alert("Число не потрапляє в діапазон");
+}
+if(hmmm >= min){
+alert("Число потрапляє в діапазон");
+}
+
+
+
+// ДД7
+
+
+
+// ДД8
+
+
+
+// ДД9
+
+
+
+// ДД10
+if ("0") {
+    console.log( 'Привіт');
+   }
+
+// ДД11
--- js/iedx2.js
+++ js/iedx2.js
@@ -138,3 +138,77 @@ console.log(a);
 console.log(b); 
 console.log(c); 
 console.log(d); 
+
+
+
+
+// ЬЬ1
+const emeil ="linnikadd@gmail.com";
+const provsrca1 = "@";
+const provsrcaemail = "email";
+const oma = emeil.length;
+console.log(emeil.includes(provsrca1));
+console.log(emeil.includes(provsrcaemail));
+console.log(oma);
+
+
+
+
+// ЬЬ2
+const myyy = "My";
+const nameeee = "name";
+const isssss = "is";
+const fullName =  myyy +  " "  + nameeee + " " +  isssss  + " " + "Victor";
+console.log(fullName);
+
+// ЬЬ3
+const userName = "Stepan";
+const payment = 100;
+const rrr = alert(`Дякуємо, ${userName} До сплати ${payment} гривень`);
+// ЬЬ4
+let meName = "Stepan";
+const ralase = "@";
+const relesee = meName.replace("S","@");
+console.log(relesee);
+
+// ЬЬ5
+const nemeMe = "Степан Ричіхін";
+const checneme = nemeMe.indexOf(" ");
+console.log(checneme);
+
+// ЬЬ6
+let nile = "ТУТ ЗАРА БУДУ 10 НУЛІВ";
+nile = nile.padEnd(32, "0");
+console.log(nile);
+const enile = nile.length;
+console.log(enile);
+
+// ЬЬ7
+const  result = 5+5+"5";
+console.log(result);
+// ЬЬ8
+const productName = "Droid";
+const pricePerItem = 3500;
+const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+
+// ЬЬ9
+const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFee = 50;
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+const infomessage = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+console.log(infomessage);
+// ЬЬ10
+const courseTopic = "JavaScript essentials";
+const courseTopicLength = courseTopic.length;
+const firstElement = courseTopic[0];
+const lastElement = courseTopic[courseTopicLength - 1];
+console.log(courseTopic);
+console.log(courseTopicLength);
+console.log(firstElement);
+console.log(lastElement);
+// ЬЬ11
+const string = "promocode Pro22812";
+const length = 9;
+const sudddd = string.slice(0, length);
+console.log(sudddd);
