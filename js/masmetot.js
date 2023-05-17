168454fb2ef40e1c6760423c28f5d62c644ca917
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-05-17 23:05:05 +0300

    hw-mas-2

---
+++ js/masmetot.js
@@ -0,0 +1,61 @@
+// mas-hw-metot-1
+// let fuhh = [1, 2, 3];
+// fuhh.splice(3, 0, 4, 5, 6);
+// console.log(fuhh); 
+
+// let fuhh2 = [1, 2, 3];
+// fuhh2.splice(0, 0, 4, 5, 6);
+// console.log(fuhh2); 
+
+
+// let fuhh3 =  ['js', 'css', 'jq'];
+// console.log(fuhh3[0]); 
+
+
+// let fuhh4 =  ['js', 'css', 'jq'];
+// console.log(fuhh4[2]); 
+
+
+// let fuhh5 = [1, 2, 3, 4, 5];
+// fuhh5.splice(0, 0,1, 2, 3);
+// console.log(fuhh5);
+
+// let fuhh5 = [1, 2, 3, 4, 5];
+// fuhh5.splice(0, 0, 4, 5);
+// console.log(fuhh5);
+
+
+
+// mas-hw-metot-2
+// const fruit = ["apple", "orenge", "cherry", "pineapple", "peach", "plum"];
+
+
+// const deletedFruir = fruit.splice(0, 1);
+
+// const deletedFruir2 = fruit.splice(4, 1);
+
+// fruit.splice(0, 0,"kilo");
+
+// fruit.splice(7, 0,"dragon");
+
+
+
+// const deletedFruir3 = fruit.splice(2, 1);
+
+// fruit.splice(2, 0,"fenom", "tiger", "magma");
+
+// console.log(fruit);
+// mas-hw-metot-3
+const styles = ['Джаз' , 'Блюз'];
+styles.splice(3, 0,'Рок-н-ролл');
+styles.splice(1, 1,'Класика');
+const deleted = styles.splice(0, 1);
+console.log(deleted);
+styles.splice(0, 1, 'Реп', 'Реггі');
+
+
+console.log(styles);
+
+
+
+
