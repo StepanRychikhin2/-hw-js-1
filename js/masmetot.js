99a9fb8f97cef97e1b12d05ba1fe158974a84dde
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-05-25 19:08:58 +0300

    hw9[updete]

--- js/masmetot.js
+++ js/masmetot.js
@@ -1,61 +1,74 @@
 // mas-hw-metot-1
+// 1.1
 // let fuhh = [1, 2, 3];
 // fuhh.splice(3, 0, 4, 5, 6);
 // console.log(fuhh); 
-
+// 1.2
 // let fuhh2 = [1, 2, 3];
 // fuhh2.splice(0, 0, 4, 5, 6);
 // console.log(fuhh2); 
 
-
+// 1.3
 // let fuhh3 =  ['js', 'css', 'jq'];
-// console.log(fuhh3[0]); 
-
-
+// console.log(fuhh3.slice(0, 1)); 
+// console.log(fuhh3[0]);
+// 1.4
 // let fuhh4 =  ['js', 'css', 'jq'];
-// console.log(fuhh4[2]); 
+// console.log(fuhh4.slice(2, 3)); 
+// console.log(fuhh4.length -1);
+// console.log(fuhh4[fuhh4.length -1]);
 
-
+// 1.6
 // let fuhh5 = [1, 2, 3, 4, 5];
-// fuhh5.splice(0, 0,1, 2, 3);
-// console.log(fuhh5);
-
-// let fuhh5 = [1, 2, 3, 4, 5];
-// fuhh5.splice(0, 0, 4, 5);
-// console.log(fuhh5);
+// console.log(fuhh5.slice(0, 3));
+// 1.7
+// let fuhh6 = [1, 2, 3, 4, 5];
+// console.log(fuhh6.slice(3));
 
 
 
 // mas-hw-metot-2
 // const fruit = ["apple", "orenge", "cherry", "pineapple", "peach", "plum"];
-
-
-// const deletedFruir = fruit.splice(0, 1);
-
-// const deletedFruir2 = fruit.splice(4, 1);
-
-// fruit.splice(0, 0,"kilo");
-
-// fruit.splice(7, 0,"dragon");
-
-
-
-// const deletedFruir3 = fruit.splice(2, 1);
-
-// fruit.splice(2, 0,"fenom", "tiger", "magma");
-
+// const deletedFruir = fruit.splice(0, 1, "kilo");
+// const deletedFruir2 = fruit.splice(5, 1, "dragon");
+// fruit.splice(2, 1,"fenom", "tiger", "magma");
 // console.log(fruit);
 // mas-hw-metot-3
-const styles = ['Джаз' , 'Блюз'];
-styles.splice(3, 0,'Рок-н-ролл');
-styles.splice(1, 1,'Класика');
-const deleted = styles.splice(0, 1);
-console.log(deleted);
-styles.splice(0, 1, 'Реп', 'Реггі');
+// const styles = ['Джаз' , 'Блюз'];
+// styles.push('Рок-н-ролл');
+// styles.splice(1, 1,'Класика');
+// console.log(styles.splice(0,1));
+// styles.splice(0, 1, 'Реп', 'Реггі');
+// console.log(styles);
 
 
-console.log(styles);
 
+
+
+
+
+
+// методи pop(), push(),shift(),unshift()
+
+
+// const test = [1, 2, 3, 4, 5, 6, 7];
+// console.log(test.pop());
+// console.log(test);
+
+
+// console.log(test.push(3));
+// console.log(test);
+
+
+
+
+// console.log(test.shift());
+// console.log(test);
+
+
+
+// console.log(test.unshift(13));
+// console.log(test);
 
 
 
