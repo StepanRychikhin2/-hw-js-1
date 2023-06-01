256968cca04b076606a7cf3a132940e39d36a108
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-06-01 19:09:14 +0300

    nohw11

---
+++ js/11hw.js
@@ -0,0 +1,84 @@
+// fohh1
+
+// const logItems = function (array) {
+//     for(let i = 0; i < array.length; i += 1){
+//         const index = i + 1;
+//         console.log(`${index}-${array[i]}`);
+//         }
+//   };
+  
+// logItems(['h', 't', 'v']);
+   
+
+// fohh2
+
+// const calculateEngravingPrice = function (message, pricePerWord) {
+//     // console.log(message.split(" ")); 
+//     const mass = message.split(" ");;
+//     const massLength = mass.length;
+//     // console.log(massLength);
+//     const totalPrise = massLength.length * pricePerWord;
+//     console.log(totalPrise);
+
+//   };
+  
+//   /*
+//    * Виклич функції для перевірки працездатності твоєї реалізації.
+//    */
+//   console.log(
+//     calculateEngravingPrice(
+//       'Proin sociis natoque et magnis parturient montes mus',
+//       10,
+//     ),
+//   ); // 80
+  
+//   console.log(
+//     calculateEngravingPrice(
+//       'Proin sociis natoque et magnis parturient montes mus',
+//       20,
+//     ),
+//   ); // 160
+  
+//   console.log(
+//     calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+//   ); // 240
+  
+//   console.log(
+//     calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20), ); // 120
+
+
+// fohh3
+
+// const findLongestWord = function (string) {
+//     let id = string.split(" ")
+//     let faseid = id[0];
+//     for(let i = 0; i < id.length; i++){
+//         if(id[i].length > faseid.length){
+//             faseid = id[i];
+//         }
+//     }
+//     return faseid;
+//   };
+  
+//   /*
+//    * Виклич функції для перевірки працездатності твоєї реалізації.
+//    */
+//   console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+  
+//   console.log(findLongestWord('Google do a roll')); // 'Google'
+  
+//   console.log(findLongestWord('May the force be with you')); // 'force'
+
+
+
+
+// fohh4
+
+
+
+
+// fohh5
+
+
+
+// fohh6
