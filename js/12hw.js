732dd718e66dd38cc4367ec21f6666651c3dd34d
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-06-15 18:39:34 +0300

    hw12

---
+++ js/12hw.js
@@ -0,0 +1,130 @@
+// завдання-1
+function makePizza() {
+
+    return "Your pizza is being prepared, please wait.";
+
+}
+
+// Change code below this line
+
+
+const result = null;
+const res = result.push(makePizza);
+const pointer = null;
+console.log(result);
+
+// завдання-2
+// function deliverPizza(pizzaName) {
+
+//     return `Delivering ${pizzaName} pizza.`;
+
+//   }
+
+//   function makePizza(pizzaName) {
+
+//     return `Pizza ${pizzaName} is being prepared, please wait...`;
+
+//   }
+
+//   // Chande code below this line
+
+//   function makeMessage(pizzaName) {
+
+//     return;
+
+//   }
+
+
+
+// завдання-3
+// function makePizza(pizzaName, callback) {
+
+//     console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+
+//     callback(pizzaName);
+
+//   }
+
+//   makePizza("Royal Grand", function deliverPizza(pizzaName) {
+
+//     console.log(`Delivering pizza ${pizzaName}.`);
+
+//   });
+// function eitPizza (pizzaName){
+//     const eating = "banana";
+// }
+//   // Change code below this line
+
+//   makePizza("Ultracheese", eitPizza);
+//   makePizza("Ultracheese", function eitPizza(pizzaName){
+// console.log(`Зїдання пітци ${pizzaName}`);
+//   })
+
+// завдання-4
+// const buttonData = document.querySelector("[button-data]");
+// buttonData.addEventListener("click", showInfo);
+// function showInfo(){
+//     console.log(alert("Hello World!"));
+// }
+
+
+
+
+// завдання-5
+// const buttonData = document.querySelector("[button-data2]");
+// buttonData.addEventListener("click", showInfo);
+// function showInfo(rety) {
+
+//     console.log("Hello World!", rety);
+//     function showInfo2() {
+//         let rety = 0;
+//         for (let i = 0; i < 999; i++) {
+//             rety + i;
+//         }
+//     }
+// }
+
+
+
+
+// завдання-6
+// const applyCallbackToEachElement = function (arr, callback) {
+//     const reuls = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         reuls.push(callback(arr[i]));
+//     }
+//     return reuls;
+
+// }
+
+
+// const squareCallback = function (arr) {
+//     return arr * arr;
+// };
+
+// const arr = [1, 2, 3, 4, 5];
+
+// const result = applyCallbackToEachElement(arr, squareCallback);
+
+// console.log(result); // [1, 4, 9, 16, 25]
+
+// завдання-7
+// const calculateDiscountedPrice = function (price, discount, callback) {
+
+
+
+// };
+
+
+
+// const showDiscountedPrice = 0;
+
+// calculateDiscountedPrice(100, 10, showDiscountedPrice); // Discounted price: 90
+
+
+
+
+
+
+
+
