96d76debb127461ad997ca497fafcaf49c442e8f
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-06-29 19:08:12 +0300

    hw14

---
+++ js/14hw.js
@@ -0,0 +1,157 @@
+// press-1
+// const myArray = {
+//     name: "Stepan",
+//     age: 46,
+//     city: "Vest",
+//     occupation: "YouTubers",
+
+// };
+// myArray.name = "NO Stepan";
+// console.log(myArray.name);
+
+// delete myArray.city
+// console.log(myArray);
+// console.log(myArray.city);
+
+// press-2
+// const shop = {
+//     predmet: [
+//         {
+//             name: "Огірок",
+//             un: "",
+//             prise: 23,
+//             bul: true,
+//             nill: null,
+//         },
+//         {
+//             name: "Помідор",
+//             un: "",
+//             prise: 455,
+//             bul: true,
+//             nill: null,
+//         },
+//         {
+//             name: "Ананас",
+//             un: "",
+//             prise: 78.23,
+//             bul: false,
+//             nill: null,
+//         },
+//     ],
+// };
+
+
+
+// press-3
+// const apartment = {
+//     imgUrl: "https://via.placeholder.com/640x480",
+//     descr: "Spacious apartment in the city center",
+//     rating: 4,
+//     price: 2153,
+
+//     tags: [
+//         {
+//             tagss: "premium",
+//             tagss: "promoted",
+//             tagss: "top",
+//         },
+//     ],
+//     // завд-3.2
+
+//     owner: [
+//         {
+//             name: "Henry",
+//             phone: 982 - 126 - 1588,
+//             email: "henry.carter@aptmail.com",
+//         },
+//     ],
+   
+// };
+
+ // завд-3.3
+
+// const aptRating = apartment.rating;
+
+// const aptDescr = apartment.descr;
+
+// const aptPrice = apartment.price;
+
+// const aptTags = apartment.tags;
+
+// завд-3.4
+// const ownerName = apartment.owner.name;
+
+// const ownerPhone = apartment.owner.phone;
+
+// const ownerEmail = apartment.owner.email;
+
+// const numberOfTags = apartment.tags.length;
+
+// const firstTag = apartment.tags[0];
+
+// const lastTag = apartment.tags[apartment.tags.length -1];
+// console.log(lastTag);
+
+// завд-3.5
+// const aptRating = apartment["rating"];
+// console.log(aptRating);
+
+// const aptDescr = apartment["descr"];
+
+
+// const aptPrice = apartment["price"];
+
+
+// const aptTags = apartment["tags"];
+// console.log(aptTags);
+
+
+// завд-3.6
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = "Henry Sibola";
+// apartment.tags.push("trusted");
+// // завд-3.7
+// apartment.area = 60,
+// apartment.rooms = 60,
+// apartment.location  = {
+//     citi: "Kingston",
+//     country: "Jamaica",
+// }
+
+// press-4
+// const naame = "Repair Droid";
+
+// const price = 2500;
+
+// const image = "https://via.placeholder.com/640x480";
+
+// const tags = ["on sale", "trending", "best buy"];
+
+ 
+
+// const product = {
+//     naame,
+//     price,
+//     image,
+//     tags,
+// };
+
+// console.log(product);
+
+
+
+
+// press-5
+// const emailInputName = "email";
+
+// const passwordInputName = "password";
+
+// const credentials = {
+
+//  [emailInputName]: "henry.carter@aptmail.com",
+//  [passwordInputName]: "password",
+
+ 
+
+// };
