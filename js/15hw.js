48e28773fad7892cfdb4eef813ac6472f8b98f47
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-07-06 19:35:23 +0300

    hw15 70%

---
+++ js/15hw.js
@@ -0,0 +1,100 @@
+// soft-1
+const user = {
+    hobby: "drave",
+    premium: true,
+};
+
+
+// user.mood = "happy";
+// user.hobby = "skydiving";
+// user.premium = false;
+
+// for(const i of Object.keys(user)){
+//     console.log(`номер ${i}: ${user}`);
+// }
+
+
+
+// soft-2
+// const countProps = function (obj) {
+//     Object.keys(obj).length
+// };
+
+// soft-3
+
+
+
+
+// soft-4
+// function countTotalSalary(employees) {
+//     let totalSelari = 0;
+//     for(const emp in employees){
+//         totalSelari += employees[emp]
+//     }
+//     return totalSelari;
+// }
+// const users = {
+//     Stepan: 1300,
+//     Volodi: 100,
+//     Pro: 560,
+// };
+// console.log(countTotalSalary(users));
+
+
+
+
+
+
+// soft-5
+const arr = [
+    {
+        neme: "orenge",
+        prise: 12,
+    },
+    {
+        neme: "wotermelon",
+        prise: 34,
+    },
+    {
+        neme: "pineapple",
+        prise: 50,
+    },
+];
+
+function getAllPropValues(arr, prop) {
+
+}
+
+getAllPropValues(arr, "123");
+
+
+// soft-6
+
+function calculateTotalPrice(allProdcuts, productName){
+
+}
+
+
+
+const allProdcuts = [
+    {
+        neme: "orenge",
+        prise: 12,
+    },
+    {
+        neme: "wotermelon",
+        prise: 34,
+    },
+    {
+        neme: "pineapple",
+        prise: 50,
+    },
+];
+
+
+
+calculateTotalPrice(neme, prise);
+
+
+
+
