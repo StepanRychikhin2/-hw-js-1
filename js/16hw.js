c44afa4d462cefe2b4b7a66f958881555b42c3fa
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-07-13 19:00:55 +0300

    hw-16

---
+++ js/16hw.js
@@ -0,0 +1,135 @@
+// soft-1
+// const user = {
+//     hobby: "drave",
+//     premium: true,
+// };
+
+// const {mood , hobby, premium} = user;
+// mood = "happy";
+// hobby = "skydiving";
+// premium = false;
+
+// for(const i of Object.keys(user)){
+//     console.log(`номер ${i}: ${user}`);
+// }
+
+
+
+// soft-2
+// const countProps = function (obj) {
+//     Object.keys(obj).length
+// };
+
+// soft-3
+
+function findBestEmployee(employees) {
+    let totalSelari = employees.stepan;
+ const keys = Object.keys(employees);
+    for(const emp of keys){
+    
+if(totalSelari < emp){
+continue;
+} else {
+totalSelari = emp;
+}
+
+    } 
+    
+    
+    return totalSelari;
+}
+const worcsUsers = {
+    stepan: 45,
+    Volodi: 67,
+    Pro: 100,
+};
+console.log(findBestEmployee(worcsUsers));
+
+
+// soft-4
+// function countTotalSalary(employees) {
+//     let totalSelari = 0;
+//     for(const emp in employees){
+//         totalSelari += employees[emp]
+//     }
+//     return totalSelari;
+// }
+// const users = {
+//     Stepan: 1300,
+//     Volodi: 100,
+//     Pro: 560,
+// };
+// console.log(countTotalSalary(users));
+
+
+
+
+
+
+// soft-5
+// const arr = [
+//     {
+//         neme: "orenge",
+//         prise: 12,
+//     },
+//     {
+//         neme: "wotermelon",
+//         prise: 34,
+//     },
+//     {
+//         neme: "pineapple",
+//         prise: 50,
+//     },
+// ];
+
+// function getAllPropValues(arr, prop) {
+
+// }
+
+// getAllPropValues(arr, "123");
+
+
+// soft-6
+
+// function calculateTotalPrice(allProdcuts, productName){
+
+// }
+
+
+
+// const allProdcuts = [
+//     {
+//         neme: "orenge",
+//         prise: 12,
+//     },
+//     {
+//         neme: "wotermelon",
+//         prise: 34,
+//     },
+//     {
+//         neme: "pineapple",
+//         prise: 50,
+//     },
+// ];
+
+
+
+// calculateTotalPrice(neme, prise);
+
+
+
+
+// soft-hw-banc
+const acaunt = {
+  muBalanse: 34000,
+};
+
+
+
+
+
+
+
+
+
+
