db3a0abad2c88cb104d360900ca96ed69081347f
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-07-27 18:43:19 +0300

    4-work

---
+++ js/18hw.js
@@ -0,0 +1,106 @@
+// hw-1
+// const user = [
+//     {
+//         name: "Stepan",
+//         age: 14,
+//     },
+//     {
+//         name: "Tolik",
+//         age: 12,
+//     },
+//     {
+//         name: "Max",
+//         age: 14,
+//     },
+// ];
+
+// const names = user.map(name2 => name2.name);
+// console.log(names);
+
+
+
+// hw-2
+// const user = [
+//     {
+//         name: "Stepan",
+//         age: 14,
+//         eyeColor: "Bly",
+//     },
+//     {
+//         name: "Tolik",
+//         age: 12,
+//         eyeColor: "Red",
+//     },
+//     {
+//         name: "Max",
+//         age: 14,
+//         eyeColor: "Blaud",
+//     },
+// ];
+// const eyeColor = user.filter(users => users.eyeColor === "Blaud");
+// console.log(eyeColor);
+
+
+// hw-3
+// const users = [
+//     {
+//         name: "Stepania",
+//         age: 14,
+//         eyeColor: "Bly",
+//         gender: "wumen",
+//     },
+//     {
+//         name: "Toliksia",
+//         age: 12,
+//         eyeColor: "Red",
+//         gender: "wumen",
+//     },
+//     {
+//         name: "Max",
+//         age: 14,
+//         eyeColor: "Blaud",
+//         gender: "man",
+//     },
+// ];
+
+// const gender1 = users.filter(userss => userss.gender === "man");
+// console.log(gender1);
+// const gender2 = users.filter(userss => userss.gender === "wumen");
+// console.log(gender2);
+
+
+
+// hw-4
+// const users = [
+//     {
+//         name: "Stepania",
+//         age: 14,
+//         eyeColor: "Bly",
+//         gender: "wumen",
+//         isActive: true,
+//     },
+//     {
+//         name: "Toliksia",
+//         age: 12,
+//         eyeColor: "Red",
+//         gender: "wumen",
+//         isActive: true,
+//     },
+//     {
+//         name: "Max",
+//         age: 14,
+//         eyeColor: "Blaud",
+//         gender: "man",
+//         isActive: false,
+//     },
+// ];
+
+// const isActive1 = users.filter(isActive1 => isActive1.isActive === true);
+// console.log(isActive1);
+// const isActive2 = users.filter(userss => userss.isActive === false);
+// console.log(isActive2);
+// hw-5
+
+
+
+// hw-6
