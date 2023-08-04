09bb97f599cb412839b07d21a97adc59d9102885
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-08-04 16:51:35 +0300

    19hw_50%

---
+++ js/19hw.js
@@ -0,0 +1,35 @@
+// womeworcs-1
+// Напишіть функцію, яка отримує масив рядків і повертає новий масив, в якому кожен 
+// з рядків складається з першої та останньої букви відповідної строки вхідного масиву.
+const array2 = ['apple', 'banana', 'orange', 'banana', 'grape', 'apple', 'kiwi'];
+
+
+// womeworcs-2
+
+// У вас є масив обʼєктів user, і у кожного з обʼєктів є name, surname та id.
+// Напишіть код, який створить ще один масив обʼєктів з параметрами id й fullName, 
+// де fullName – складається з name та surname.
+// const people = [
+// { name: 'John', surname: "Doe", age: 32, occupation: 'programmer' },
+// { name: 'Jane', surname: "Lee", age: 26, occupation: 'teacher' },
+// { name: 'Mike', surname: "Watson", age: 42, occupation: 'engineer' },
+// { name: 'Emily', surname: "Brad", age: 29, occupation: 'designer' }
+// ];
+
+// const news = [];
+// const gender1 = people.map(userss => news.push(userss.age));
+// console.log(gender1);
+// console.log(news);
+
+// const gender2 = people.map(userss => news.push(userss.name + " " + userss.surname));
+// console.log(gender1);
+// console.log(news);
+
+
+
+
+
+
+
+
+
