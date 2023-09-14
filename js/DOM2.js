2d10a165e432476f99d1c39976c4ff59ff5261b6
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-09-14 17:49:50 +0300

    hw4_DOM

---
+++ js/DOM2.js
@@ -0,0 +1,40 @@
+// worc-1
+// const p = document.body.querySelector(".p");
+
+// console.log(p);
+
+// const button = document.body.querySelector(".button");
+
+
+// console.log(button);
+
+
+// button.textContent = p.textContent;
+
+
+// worc-2
+// const img = document.body.querySelector(".img");
+
+// img.src = "./car2.jpg";
+// console.log(img);
+
+// // worc-3
+
+// const img = document.body.querySelector(".img2");
+
+// img.alt = "CAR";
+// console.log(img);
+
+
+// const adres = document.body.querySelector(".adress");
+
+// adres.href = "https://www.google.com/search?sca_esv=565338080&sxsrf=AM9HkKnIXhvPZszgdY8KPRCfJSQwA3EuRg:1694701292687&q=car&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjj4rOkpqqBAxXKFhAIHZH5CvcQ0pQJegQIDBAB&biw=1280&bih=571&dpr=1.5";
+// console.log(adres);
+
+// worc-4
+const list = document.body.querySelector(".item");
+
+list.firstChild.textContent = "pro" ;
+console.log(list);
+
+
