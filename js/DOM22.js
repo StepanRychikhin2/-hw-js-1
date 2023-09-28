eda02e38c7526cbc54168dbb4f952e0afecfb5cb
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-09-28 17:39:05 +0300

    HW-DOM-UPDETE

--- js/DOM22.js
+++ js/DOM22.js
@@ -1,24 +1,32 @@
 // WORC_1
 
-// const categoriesList = document.querySelector(".categories1");
-// const categoryItems = categoriesList.querySelectorAll('.item');
+// const categoriesList = document.querySelectorAll(".categories");
+// const categoryItems = document.querySelectorAll('.item');
+
 // console.log(categoriesList);
 // console.log(categoryItems);
 
-// const work = categoryItems.length;
-// console.log(`У категорії ${work} елементів`);
+// const work = categoriesList.length;
+// console.log(`є всього ${work} категорій`);
 
 
 // const nameAnimal = ["КІТ", "СОБАКА", "КІНЬ", "ХОМ'ЯК"];
-// const list = document.querySelector(".categories1");
+// const list = document.querySelector(".categories");
 
 // const names = nameAnimal
-//   .map((nameAnimal) => `<li class="list-item">${nameAnimal}</li>`)
+//   .map((nameAnimal) => `<li class="list-item"><h2 class="list-h2">${nameAnimal}</h2></li>`)
 //   .join("");
 
 // list.innerHTML = names;
 
 // const categoryItems2 = list.querySelectorAll('li');
+// const categiryText = document.querySelectorAll(".list-h2");
+
+// for (let i = 0; i < categiryText.length; i++) {
+//     const element = categiryText[i];
+//     console.log(element.textContent);
+// }
+
 
 // const works = categoryItems2.length;
 // console.log(`У категорії Тварини ${works} елементів`);
@@ -35,65 +43,50 @@
 //   ];
   
 //   const ulElement = document.getElementById('ingredients');
-  
-//   const liElement = document.createElement('li');
-//   liElement.textContent = ingredients[0];
+
+//   for (let i = 0; i < ingredients.length; i++) {
+//     const element = ingredients[i];
+    
+//     const liElement = document.createElement('li');
+//   liElement.textContent = ingredients[i];
 //   ulElement.appendChild(liElement);
+//   }
 
 
 
-//   const liElement2 = document.createElement('li');
-//   liElement2.textContent = ingredients[1];
-//   ulElement.appendChild(liElement2);
-
-//   const liElement3 = document.createElement('li');
-//   liElement3.textContent = ingredients[2];
-//   ulElement.appendChild(liElement3);
-
-//   const liElement4 = document.createElement('li');
-//   liElement4.textContent = ingredients[3];
-//   ulElement.appendChild(liElement4);
-
-//   const liElement5 = document.createElement('li');
-//   liElement5.textContent = ingredients[4];
-//   ulElement.appendChild(liElement5);
-
-//   const liElement6 = document.createElement('li');
-//   liElement6.textContent = ingredients[5];
-//   ulElement.appendChild(liElement6);
-
+ 
 
 
 // WORC_3
 
 
 
-//   const images = [
-//     {
-//       url:
-//         'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//       alt: 'White and Black Long Fur Cat',
-//     },
-//     {
-//       url:
-//         'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//       alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-//     },
-//     {
-//       url:
-//         'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-//       alt: 'Group of Horses Running',
-//     },
-//   ];
+  const images = [
+    {
+      url:
+        'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'White and Black Long Fur Cat',
+    },
+    {
+      url:
+        'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    },
+    {
+      url:
+        'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+      alt: 'Group of Horses Running',
+    },
+  ];
   
-//   const gallery = document.getElementById('gallery');
+  const gallery = document.getElementById('gallery');
   
-//   const galleryHTML = images.map((image) => `
-//     <li>
-//       <img src="${image.url}" alt="${image.alt}" width = "200px">
-//     </li>`).join('');
+  const galleryHTML = images.map((image) => `
+    <li>
+      <img src="${image.url}" alt="${image.alt}" width = "200px">
+    </li>`).join('');
   
-//     gallery.insertAdjacentHTML('beforeend', galleryHTML);
+    gallery.insertAdjacentHTML('beforeend', galleryHTML);
 
 
 
@@ -101,7 +94,12 @@
 
 // WORC_4
 
+// const buttton1 = document.;
 
+
+
+
+// element.addEventListener(event, handler[, phase])
 
 
 
