15dc809bf6a0c39ad0871735b0b6a5c696bc7ef0
StepanRychikhin2 <111581882+StepanRychikhin2@users.noreply.github.com>
2023-10-21 10:15:00 +0300

    upd

---
+++ js/keypodidi.js
@@ -0,0 +1,88 @@
+// const gallery = document.querySelector(".gallery");
+// const gallereyLi = gallery.querySelectorAll("li");
+// const imege = document.querySelectorAll("img");
+// console.log(gallery);
+// console.log(gallereyLi);
+// console.log(imege);
+
+
+
+
+
+// for (let i = 0; i < gallereyLi.length; i++) {
+//         const oneli = gallereyLi[i];
+//         document.addEventListener("keypdown", (e) => {
+//             if (e.key === "ArrowLeft") {
+//                 i + 1;
+//                 console.log(e.key);
+//                 } else if (e.key === "ArrowRight") {
+//                     i - 1;
+//                 }
+    
+//         })
+//         oneli.addEventListener("click", () => {
+//             oneli.classList.toggle("full-image-container")
+//             imege[i].classList.toggle("full-image")
+//         })
+        
+        
+//     }
+
+
+
+// const button1 = document.querySelector("[data-action1]");
+// const button2 = document.querySelector("[data-action]");
+
+
+// button1.addEventListener("click", create)
+// button2.addEventListener("click", remove)
+
+
+
+//     function randomColor() {
+//         const r = Math.floor(Math.random() * 256);
+//         const g = Math.floor(Math.random() * 256);
+//         const b = Math.floor(Math.random() * 256);
+//         return `rgb(${r}, ${g}, ${b})`;
+//       }
+  
+//       function create() {
+//         const amoun = document.getElementById('inputAmount').value;
+//         const konteiner = document.getElementById('boxes');
+  
+//         // Очищення попередніх елементів
+//         konteiner.innerHTML = '';
+  
+//         let size = 30;
+  
+//         for (let i = 0; i < amoun; i++) {
+//           const box = document.createElement('div');
+//           box.classList.add('box');
+//           box.style.backgroundColor = randomColor();
+//           box.style.width = size + 'px';
+//           box.style.height = size + 'px';
+//           konteiner.appendChild(box);
+  
+//           size += 10;
+//         }
+//       }
+  
+//       function remove() {
+//         const konteiner = document.getElementById('boxes');
+//         konteiner.innerHTML = '';
+//       }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
